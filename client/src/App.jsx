db869b33d5b6e88df0bae3741e8ad93188bdecb2
J7SUS <j7suspro@MacBook-Pro-JECM.local>
2025-01-08 22:25:02 +0100

    fix: adding try/catch - fetch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,20 @@ import { getMockDeliveries, getMockBoxes } from "./services/api";
 function App() {
   const [deliveries, setDeliveries] = useState([]);
   const [boxes, setBoxes] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fecthData = async () => {
-      const mockBoxes = await getMockBoxes();
-      const mockDeliveries = await getMockDeliveries();
-      setDeliveries(mockDeliveries);
-      setBoxes(mockBoxes);
+
+      try {
+        const mockBoxes = await getMockBoxes();
+        const mockDeliveries = await getMockDeliveries();
+        setDeliveries(mockDeliveries);
+        setBoxes(mockBoxes); 
+      } catch (error) {
+        setError("Error loading data")
+      }
+      
     };
     fecthData();
   }, []);
